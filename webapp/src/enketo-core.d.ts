0205b1ed341d8ad0813f8aef845ef03c88c92a7d
Joshua Kuestersteffen <jkuester@kuester7.com>
2026-04-10 10:15:55 -0500

    Move types file into new `src/types` directory

--- webapp/src/enketo-core.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'enketo-core/src/js/event' {
-  const events: {
-    BeforeSave: () => CustomEvent;
-    [key: string]: (...args: unknown[]) => Event;
-  };
-  export default events;
-}
